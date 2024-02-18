66540a265f41ed038cd60c7ec8941bf1c5817ef9
Julien Pactat <julien.pactat.akyos@gmail.com>
2024-02-18 21:42:01 +0100

    Creator dashboard

---
+++ app/(dashboard)/u/[username]/layout.tsx
@@ -0,0 +1,28 @@
+import { getSeflByUsername } from "@/lib/auth-service";
+import { redirect } from "next/navigation";
+import Navbar from "./_components/navbar";
+import Sidebar from "./_components/sidebar";
+import Container from "./_components/container";
+
+interface ICreatorLayoutProps {
+  params: { username: string };
+  children: React.ReactNode;
+}
+
+const CreatorLayout = async ({ params, children }: ICreatorLayoutProps) => {
+  const self = await getSeflByUsername(params.username);
+
+  if (!self) redirect("/");
+
+  return (
+    <>
+      <Navbar />
+      <div className="flex h-full pt-20">
+        <Sidebar />
+        <Container>{children}</Container>
+      </div>
+    </>
+  );
+};
+
+export default CreatorLayout;
